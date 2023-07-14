bd15acccbc89c9a76921fc16022c86eb3aca6c2d
Carolina <96888706+Carowa27@users.noreply.github.com>
2023-07-14 19:21:04 +0200

    Update Router.tsx via github app

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,6 +8,9 @@ export const Router = createBrowserRouter([
     path: "/",
     element: <Layout></Layout>,
     errorElement: <Error></Error>,
-    children: [{ path: "/", element: <Home></Home>, index: true }],
+    children: [{ path: "/", element: <Home></Home>, index: true },
+{path:"/shows",element:<Shows></Shows>},
+{path:"/producers",element:<Producers></Producers>},
+{path:"/community",element:<Community></Community>}],
   },
 ]);
