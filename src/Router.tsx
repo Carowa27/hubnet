e72f19f0e3fc09373aa536174de9706ab22b1491
Carolina Warntorp <carolina.warntorp@outlook.com>
2023-07-14 14:51:13 +0200

    (build) add router and layout

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "./Layout";
+import { Error } from "./Error";
+import { Home } from "./Home";
+
+export const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    errorElement: <Error></Error>,
+    children: [{ path: "/", element: <Home></Home>, index: true }],
+  },
+]);
