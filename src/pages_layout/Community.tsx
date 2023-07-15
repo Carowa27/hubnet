747a476554b06fbc089e4369afb70e529d9f9fbe
Carolina <96888706+Carowa27@users.noreply.github.com>
2023-07-15 21:09:00 +0200

    (build) add video

--- src/pages_layout/Community.tsx
+++ src/pages_layout/Community.tsx
@@ -7,18 +7,30 @@ export const Community = () => {
       <div className="community-header-container">
         <h1 className="community-header">Get Connected</h1>
       </div>
-      <div className="connect-container">
-        <p>Join our discord channel</p>
-        <div className="img-container">
-          <img className="img-discord" src={DiscordLogo} alt="discord logo" />
+      <div className="community-container">
+        <div className="connect-container">
+          <p>Join our discord channel</p>
+          <div className="img-container">
+            <img className="img-discord" src={DiscordLogo} alt="discord logo" />
+          </div>
         </div>
-      </div>
-      <div className="voiceacting-container">
-        <p>
-          Flixmix start your acting carier! If you want to know more watch this
-          video
-        </p>
-        <div className="video-container">VIDEO</div>
+        <div className="voiceacting-container">
+          <p>
+            Flixmix start your acting carier! If you want to know more watch
+            this video
+          </p>
+          <div className="video-container">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/uS_ADGEJSfI"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
       </div>
     </>
   );
