395fc86018a7a77977e90226680558e6b2e5f5e9
Carolina <carolina.warntorp@outlook.com>
2024-07-14 17:31:37 +0200

    (ref) remove main word from main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { HubwireGalactic } from "@/data/newly created files/ZarkMedia";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      main
       <ShowCard show={HubwireGalactic} homePage={true} />
     </main>
   );
