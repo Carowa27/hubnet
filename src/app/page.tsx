48915ae43ffa5fa16202b49fe253781f1a92f15b
Carowa27 <carolina.warntorp@outlook.com>
2024-07-15 13:25:00 +0200

    (build) add ProdLogoCarousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { ShowCard } from "@/components/Cards";
 import Image from "next/image";
 import { HubwireGalactic } from "@/data/newly created files/ZarkMedia";
-import { ShowCarousel } from "@/components/Carousels";
+import { ProdLogoCarousel, ShowCarousel } from "@/components/Carousels";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ShowCarousel homePage={true} />
-      {/* <ShowCard show={HubwireGalactic} homePage={true} /> */}
     </main>
   );
 }
--- src/components/Carousels.tsx
+++ src/components/Carousels.tsx
@@ -104,3 +104,102 @@ export const ShowCarousel = ({ homePage }: ShowCarouselParams) => {
     </div>
   );
 };
+
+export const ProdLogoCarousel = () => {
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(4);
+  const rotateFunction = () => {};
+  return (
+    <>
+      <section>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          {Shows.map((show, i) => {
+            if (i >= start && i <= end) {
+              return i === start || i === end ? (
+                //0 & 4
+                <div
+                  key={i}
+                  style={{
+                    boxShadow: "0px 0px 5px 0px #5495D5",
+                    border: "1px solid #5495D5",
+                    borderRadius: "10px",
+                    aspectRatio: "1/1",
+                    width: "50px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginLeft: "5px",
+                    marginRight: "5px",
+                  }}
+                >
+                  <img
+                    style={{
+                      aspectRatio: "1/1",
+                      width: "40px",
+                    }}
+                    src={show.producer.logo}
+                    alt={`${show.name} logo`}
+                  />
+                </div>
+              ) : i === start + 1 || i === end - 1 ? (
+                //1 & 3
+                <div
+                  key={i}
+                  style={{
+                    boxShadow: "0px 0px 5px 0px #5495D5",
+                    border: "1px solid #5495D5",
+                    borderRadius: "10px",
+                    aspectRatio: "1/1",
+                    width: "70px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginLeft: "5px",
+                    marginRight: "5px",
+                  }}
+                >
+                  <img
+                    style={{
+                      aspectRatio: "1/1",
+                      width: "60px",
+                    }}
+                    src={show.producer.logo}
+                    alt={`${show.name} logo`}
+                  />
+                </div>
+              ) : (
+                //2 (center)
+                <div
+                  key={i}
+                  style={{
+                    boxShadow: "0px 0px 5px 0px lightGray",
+                    border: "1px solid lightGray",
+                    borderRadius: "10px",
+                    aspectRatio: "1/1",
+                    width: "90px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginLeft: "5px",
+                    marginRight: "5px",
+                  }}
+                >
+                  <img
+                    style={{
+                      aspectRatio: "1/1",
+                      width: "80px",
+                    }}
+                    src={show.producer.logo}
+                    alt={`${show.name} logo`}
+                  />
+                </div>
+              );
+            } else return;
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+////
