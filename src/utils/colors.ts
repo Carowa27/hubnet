e01ad3ba50a066e02fd5a229f29c23dbac5cae0e
Carolina <carolina.warntorp@outlook.com>
2024-07-18 19:46:36 +0200

    (ref) add color variables to color file (styled)

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,5 +1,23 @@
 export const color = {
-  white: "#ffffff",
+  //  shades
+  background: "#0e1015",
+  primary: "#474e67",
+  backgroundComponentProducerTop: "#2b2f3e",
+  borderProducerNotFocusedHeader: "#4d628c",
+  // tints
+  paragraphColor: "#b5b8c2",
+  paragraphFooter: "#9195a4",
   buttonWhiteBackground: "#d6e1f7",
+  white: "#ffffff",
+  // gradients
+  backgroundFooterAndHeader: "linear-gradient(to right, #1c1f29, #323748)",
+  backgroundCardHome: "linear-gradient(to bottom, #2b2f3e, #242734)",
+  backgroundButton: "linear-gradient(to bottom, #474e67, #2b2f3e)",
+  backgroundComponentProducerBottom:
+    "linear-gradient(to bottom,#1c1f29,#07080a)",
+  // overlay fades
+  overlayCardFadeDefault: "linear-gradient(to bottom, #2b2f3e00, #2b2f3e)",
+  overlayCardFadeHover: "linear-gradient(to bottom, #2b2f3ecc, #2b2f3e)",
+  // other
   darkBlue50Opacity: "#090b1150",
 };
