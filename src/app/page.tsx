3acb107166154b402627e619155a45b3e3fcd8cb
Carowa27 <carolina.warntorp@outlook.com>
2024-07-25 10:58:44 +0200

    (ref) remove cards from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { ShowsCarousel } from "@/components/carousels/ShowsCarousel";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <JoinCommunityCard />
       <ShowsCarousel homePage={true} />
     </main>
   );
